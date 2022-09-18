5618fe3b6af9cc8afb3573090c57f5513b848092
Glitch (magnificent-silk-pie) <none>
2022-09-18 15:29:49 +0000

    🎽❄️ Checkpoint ./events/ready.js:48888774/32

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 module.exports = (client, interaction) => {
     console.log(`${client.user.tag} ismi ile giriş yapıldı`);
-    client.user.setPresence({activities: [{name:"♡ Developed By Vibe"}], status:"online"});   
+    client.user.setPresence({activities: [{name:"K A P L A N"}], status:"online"});   
 };
